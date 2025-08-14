e4c3ce48561424c12fc30becbdb982cc40b2a610
Matheus Macedo <101157827+mattzu22@users.noreply.github.com>
2025-08-14 18:43:17 -0300

    aula 3

---
+++ src/app.ts
@@ -0,0 +1,44 @@
+import fastify from 'fastify'
+import { fastifySwagger } from '@fastify/swagger'
+import { validatorCompiler, serializerCompiler, type ZodTypeProvider, jsonSchemaTransform } from 'fastify-type-provider-zod'
+import { createCourseRoute } from './routes/create-course.ts'
+import { getCourseByIdRoute } from './routes/get-course-by-id.ts'
+import { getCoursesRoute } from './routes/get-courses.ts'
+import scalarAPIReference from '@scalar/fastify-api-reference'
+
+const server = fastify({
+  logger: {
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        translateTime: 'HH:MM:ss Z',
+        ignore: 'pid,hostname',
+      },
+    },
+  },
+}).withTypeProvider<ZodTypeProvider>()
+
+if (process.env.NODE_ENV === 'development') {
+  server.register(fastifySwagger, {
+    openapi: {
+      info: {
+        title: 'Desafio Node.js',
+        version: '1.0.0',
+      }
+    },
+    transform: jsonSchemaTransform,
+  })
+  
+  server.register(scalarAPIReference, {
+    routePrefix: '/docs',
+  })
+}
+
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
+
+server.register(createCourseRoute)
+server.register(getCourseByIdRoute)
+server.register(getCoursesRoute)
+
+export { server }
